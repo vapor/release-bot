426ebfb2a8f1805f028526d79b47e1471b4be690
Tim <0xtimc@gmail.com>
2021-03-23 13:28:45 +0000

    Specify v1 of API Gateway

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -7,5 +7,6 @@ try LoggingSystem.bootstrap(from: &env)
 let app = Application(env)
 defer { app.shutdown() }
 try configure(app)
+app.lambda.server.configuration = .init(apiService: .apiGateway, logger: app.logger)
 app.servers.use(.lambda)
 try app.run()
