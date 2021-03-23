772305bb8c579ee5e4454cc902b788046ffddfcc
Tim <0xtimc@gmail.com>
2021-03-23 10:20:21 +0000

    Add support for AWS Lambda

--- Package.swift
+++ Package.swift
@@ -8,11 +8,13 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor-community/vapor-aws-lambda-runtime", .upToNextMajor(from: "0.4.0")),
     ],
     targets: [
         .target(name: "App", dependencies: [
             .product(name: "Vapor", package: "vapor"),
+            .product(name: "VaporAWSLambdaRuntime", package: "vapor-aws-lambda-runtime"),
         ]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: [
--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,9 +1,11 @@
 import App
 import Vapor
+import VaporAWSLambdaRuntime
 
 var env = try Environment.detect()
 try LoggingSystem.bootstrap(from: &env)
 let app = Application(env)
 defer { app.shutdown() }
 try configure(app)
+app.servers.use(.lambda)
 try app.run()
